7017c254f0cb2b2963e2e5264d90429db694897d
Marcelo Rafael Gonçalves <marcelo.rafael.goncalves@gmail.com>
2021-06-23 13:38:13 -0300

    contexto de autenticação

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,4 +1,7 @@
+import { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
+
+import { AuthContext } from '../App';
 
 import illustrationImg from '../assets/illustration.svg';
 import logoImg from '../assets/logo.svg';
@@ -6,21 +9,17 @@ import googleIconImg from '../assets/google-icon.svg';
 
 import { Button } from '../components/Button';
 
-import { auth, firebase } from '../services/firebase';
-
 import '../styles/auth.scss';
 
 export function Home() {
   const history = useHistory();
+  const { user, signInWithGoogle } = useContext(AuthContext);
 
-  function handleCreateRoom() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-
-    auth.signInWithPopup(provider).then(result => {
-      console.log(result);
-
-      history.push('./rooms/new');
-    });
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle();
+    }
+    history.push('./rooms/new');
   }
 
   return (
