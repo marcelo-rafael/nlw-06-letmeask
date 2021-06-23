5ea82ced3bf27acf95a8735b8a60d12f6f0b36fb
Marcelo Rafael Gonçalves <marcelo.rafael.goncalves@gmail.com>
2021-06-23 00:49:19 -0300

    autenticação com firebase

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -6,13 +6,21 @@ import googleIconImg from '../assets/google-icon.svg';
 
 import { Button } from '../components/Button';
 
+import { auth, firebase } from '../services/firebase';
+
 import '../styles/auth.scss';
 
 export function Home() {
   const history = useHistory();
 
-  function navigateToNewRoom() {
-    history.push('./rooms/new');
+  function handleCreateRoom() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider).then(result => {
+      console.log(result);
+
+      history.push('./rooms/new');
+    });
   }
 
   return (
@@ -28,7 +36,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Logo do Letmeask" />
-          <button onClick={navigateToNewRoom} className="create-room">
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
