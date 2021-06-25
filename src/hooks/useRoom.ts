f992384d5d69b922ec477f82adaa15e3419962d8
Marcelo Rafael Gonçalves <marcelo.rafael.goncalves@gmail.com>
2021-06-24 22:45:04 -0300

    funcionalidade do like

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,16 +1,6 @@
 import { useEffect, useState } from 'react';
 import { database } from '../services/firebase';
-
-type QuestionType = {
-  id: string;
-  author: {
-    name: string;
-    avatar: string;
-  };
-  content: string;
-  isAnswered: boolean;
-  isHighlighted: boolean;
-};
+import { useAuth } from './useAuth';
 
 type FirebaseQuestions = Record<
   string,
@@ -22,10 +12,30 @@ type FirebaseQuestions = Record<
     content: string;
     isAnswered: boolean;
     isHighlighted: boolean;
+    likes: Record<
+      string,
+      {
+        authorId: string;
+      }
+    >;
   }
 >;
 
+type QuestionType = {
+  id: string;
+  author: {
+    name: string;
+    avatar: string;
+  };
+  content: string;
+  isAnswered: boolean;
+  isHighlighted: boolean;
+  likeCount: number;
+  likeId: string | undefined;
+};
+
 export function useRoom(roomId: string) {
+  const { user } = useAuth();
   const [questions, setQuestions] = useState<QuestionType[]>([]);
   const [title, setTitle] = useState('');
 
@@ -43,14 +53,22 @@ export function useRoom(roomId: string) {
             content: value.content,
             author: value.author,
             isHighlighted: value.isHighlighted,
-            isAnswered: value.isAnswered
+            isAnswered: value.isAnswered,
+            likeCount: Object.values(value.likes ?? {}).length,
+            likeId: Object.entries(value.likes ?? {}).find(
+              ([key, like]) => like.authorId === user?.id
+            )?.[0]
           };
         }
       );
       setTitle(databaseRoom.title);
       setQuestions(parsedQuestions);
     });
-  }, [roomId]);
+
+    return () => {
+      roomRef.off('value');
+    };
+  }, [roomId, user?.id]);
 
   return { questions, title };
 }
